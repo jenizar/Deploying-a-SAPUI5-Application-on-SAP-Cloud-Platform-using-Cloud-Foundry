44e6f5738a4bd2ef27de2cddbea775fe662d1dab
jenizar <jenizar@hotmail.com>
2019-01-14 20:22:36 +0700

    my first commit

---
+++ dist/test/unit/AllTests-dbg.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sapui5_launchpad/sapui5-launchpad/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,12 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+	"use strict";
+
+	sap.ui.require([
+		"sapui5_launchpad/sapui5-launchpad/test/integration/AllJourneys"
+	], function () {
+		QUnit.start();
+	});
+});
